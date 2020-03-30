79bd3eddacc50477f509a62eeb6b91691651fd81
Reha <marian.reha@storaenso.com>
2020-03-30 19:48:17 +0200

    Basic login/logout implemented

---
+++ .history/src/components/User_20200330194136.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-if="userEmail===''" class="loginButton"><button>Login</button></div>
+        <div v-else class="loginButton"><button>Logout</button></div>
+        <div>{{ userEmail }}</div>
+    </div>
+</template>
+
+<script>
+import aad from '../services/aad';
+
+export default {
+    name: 'User',
+    components: {
+
+    },
+
+    data: () => ({
+        userEmail: '',
+    })
+}
+</script>
+
+<style>
+.loginButton {
+    padding-top: 50px;
+}
+</style>
---
+++ .history/src/services/aad_20200330194523.js
@@ -0,0 +1,39 @@
+import { UserAgentApplication } from 'msal';
+
+var msalConfig = {
+    auth: {
+        clientId: "860e0df4-3d7e-403d-b7fc-5a3bff9ee259",
+        authority: "https://login.microsoftonline.com/common",
+        redirectURI: "http://localhost:8080/"
+    },
+    cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: true
+    }
+};
+
+var requestObj = {
+    scopes: ["user.read"]
+};
+
+var myMSALObj = new UserAgentApplication(msalConfig);
+
+var login = async () => {
+    var authResult = await myMSALObj.loginPopup(requestObj);
+    return authResult.account;
+};
+
+var getAccount = async () => {
+    var account = await myMSALObj.getAccount();
+    return account;
+};
+
+var logout = async () => {
+    myMSALObj.logout();
+};
+
+export default {
+    login,
+    getAccount,
+    logout
+};
